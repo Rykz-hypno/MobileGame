35e91382cdecd4ac56e22612886e5ce77656cbea
Nevzad Elezovic <nevzad.elezovic@gmail.com>
2026-01-25 23:18:39 +0100

    Add separate smooth speeds for camera X and Y axes
    Refactored CameraScript to use independent smoothing speeds for the X and Y axes, allowing for more granular control over camera movement. Updated variables and logic to support this change.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -3,16 +3,25 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     [SerializeField] private Transform player;
-    public float SmoothSpeed = 0.1f;
+    public float SmoothSpeedX = 0.3f;
+    public float SmoothSpeedY = 0.05f;
     private Vector3 offset;
-    private Vector3 velocity;
+    private Vector3 velocityX;
+    private Vector3 velocityY;
 
     void Start()
     {
         offset = transform.position - player.position;
     }
+    
     void LateUpdate()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, player.position + offset, ref velocity, SmoothSpeed);
+        Vector3 targetPosition = player.position + offset;
+        
+        Vector3 currentPos = transform.position;
+        currentPos.x = Mathf.SmoothDamp(currentPos.x, targetPosition.x, ref velocityX.x, SmoothSpeedX);
+        currentPos.y = Mathf.SmoothDamp(currentPos.y, targetPosition.y, ref velocityY.y, SmoothSpeedY);
+        
+        transform.position = currentPos;
     }
 }
